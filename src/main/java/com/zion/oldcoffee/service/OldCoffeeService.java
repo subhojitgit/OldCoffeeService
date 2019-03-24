c34c40ec14d63078f3f7446dd8786ad58108c446
Subhojit <subhojit.ds@gmail.com>
2019-03-24 00:06:57 -0700

    Initial rest api setup

--- src/main/java/com/zion/oldcoffee/service/OldCoffeeService.java
+++ src/main/java/com/zion/oldcoffee/service/OldCoffeeService.java
@@ -1,4 +1,17 @@
 package com.zion.oldcoffee.service;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/coffee")
 public class OldCoffeeService {
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String getCoffee() {
+        return "<h2>Hello Jersey</h2>";
+    }
+
 }
